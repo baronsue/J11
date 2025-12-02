f82d19430d02493053de7aff4fd4ea590de4d53c
Baron <2806983376@q.com>
2025-12-02 10:48:42 +0800

    DTO:create AvailabilityRequest DTO

---
+++ src/main/java/com/restaurant/dto/request/AvailabilityRequest.java
@@ -0,0 +1,44 @@
+package com.restaurant.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+/**
+ * Availability query request DTO.
+ */
+public class AvailabilityRequest {
+
+    private static final int MINIMUM_GUESTS = 1;
+
+    @NotNull(message = "Date is required")
+    private LocalDate date;
+
+    @Min(value = MINIMUM_GUESTS, message = "Number of guests must be at least 1")
+    private Integer numberOfGuests;
+
+    public AvailabilityRequest() {
+    }
+
+    public AvailabilityRequest(LocalDate date, Integer numberOfGuests) {
+        this.date = date;
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(Integer numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+}
